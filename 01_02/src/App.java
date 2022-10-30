b26a481514a63a5d4cad9871cfe6cbc2cde1dacf
JKalina08 <kalina08@student.vspj.cz>
2022-10-30 19:26:30 +0000

    refaktoring

--- 01_02/src/App.java
+++ 01_02/src/App.java
@@ -1,26 +1,19 @@
 public class App {
 
+    // kalina08 - proc private metoda?
     private static void DatoveTypy(){
 
-        byte mybyte;
-        short myshort;
-        int myint;
-        long mylong;
-        float myfloat;
-        double mydouble;
-        boolean mybool;
-        char mychar;
+        byte mybyte = 0;
+        short myshort = 0;
+        int myint = 0;
+        long mylong = 0;
+        float myfloat = 0.0f;
+        double mydouble  = 0.0;
+        boolean mybool = false;
+        char mychar = 0;
+   }
 
-        mybyte = 0;
-        myshort = 0;
-        myint = 0;
-        mylong =0;
-        myfloat = 0.0f;
-        mydouble = 0.0;
-        mybool = false;
-        mychar = 0;
-    }
-
+    // kalina08 - proc private metoda?
     private static void PrintTypy(){
         final byte bits = 8;
         System.out.println("| Type \t\t\t\t| Size \t\t\t| Min \t\t\t\t\t\t\t| Max \t\t\t\t\t\t\t|");
@@ -28,7 +21,7 @@ public class App {
         System.out.println("| byte (Byte) \t\t|  " + Byte.SIZE + " B =  " + Byte.SIZE*bits + " b \t|" + " min: " + Byte.MIN_VALUE + " \t\t\t\t\t|" + " max: " + Byte.MAX_VALUE + " \t\t\t\t\t\t|");
         System.out.println("| short (Short) \t| " + Short.SIZE + " B = " + Short.SIZE*bits + " b \t|" + " min: " + Short.MIN_VALUE + " \t\t\t\t\t|" + " max: " + Short.MAX_VALUE + " \t\t\t\t\t|");
         System.out.println("| int (Integer) \t| " + Integer.SIZE + " B = " + Integer.SIZE*bits + " b \t|" + " min: " + Integer.MIN_VALUE + " \t\t\t\t|" + " max: " + Integer.MAX_VALUE + " \t\t\t\t|");
-        System.out.println("| long (Long) \t\t| " + Long.SIZE + " B = " + Long.SIZE*bits + " b \t|" + " min: " + Long.MIN_VALUE + " \t|" + " max: " + Long.MAX_VALUE + " \t\t|");
+        System.out.println("| long (Long) \t\t| " + Long.SIZE + " B = " + Long.SIZE*bits + " b \t|" + " min: " + Long.MIN_VALUE + " \t\t\t|" + " max: " + Long.MAX_VALUE + " \t\t|");
         System.out.println("| float (Float) \t| " + Float.SIZE + " B = " + Float.SIZE*bits + " b \t|" + " min: " + Float.MIN_VALUE + " \t\t\t\t\t|" + " max: " + Float.MAX_VALUE + " \t\t\t|");
         System.out.println("| double (Double) \t| " + Double.SIZE + " B = " + Double.SIZE*bits + " b \t|" + " min: " + Double.MIN_VALUE + " \t\t\t\t|" + " max: " + Double.MAX_VALUE + "\t|");
     // Zadání: U číselných datových typů bude uveden rozsah. - WTF? Vzdyt to je Min az Max. A jak mam vypsat jiny, nez ciselny typy
